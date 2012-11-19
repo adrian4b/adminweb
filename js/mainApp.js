293e6902ee17a482366e48d40d56a3382523577e
Adrian <abrezai@gmail.com>
2012-11-19 02:29:02 -0800

    some cleanup

--- js/mainApp.js
+++ js/mainApp.js
@@ -61,10 +61,6 @@ $(function() {
 	ko.applyBindings(new viewModel());
 });
 
-$(function() {
-    // setup ul.tabs to work as tabs for each div directly under div.panes
-    $("ul.tabs").tabs("div.panes > div");
-});
 
 $(document).ready(function(){
 
