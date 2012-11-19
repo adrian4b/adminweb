4a6df764b32a44f7f28dff3fdcfb984179ce80b7
Adrian <abrezai@gmail.com>
2012-11-18 23:05:34 -0800

    using another tab from jquery

--- js/mainApp.js
+++ js/mainApp.js
@@ -65,3 +65,21 @@ $(function() {
     // setup ul.tabs to work as tabs for each div directly under div.panes
     $("ul.tabs").tabs("div.panes > div");
 });
+
+$(document).ready(function(){
+
+	$("#tabs li").click(function() {
+		//	First remove class "active" from currently active tab
+		$("#tabs li").removeClass('active');
+		//	Now add class "active" to the selected/clicked tab
+		$(this).addClass("active");
+		//	Hide all tab content
+		$(".tab_content").hide();
+		//	Here we get the href value of the selected tab
+		var selected_tab = $(this).find("a").attr("href");
+		//	Show the selected tab content
+		$(selected_tab).fadeIn();
+		//	At the end, we add return false so that the click on the link is not executed
+		return false;
+	});
+});
